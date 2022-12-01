5fbb64841d7d957b7f614cf69cbf35d55c667a98
Ramesh Banakar <ramesh.s.b@campusuvce.in>
2022-12-01 11:12:04 +0530

    router and promises

--- 07_delete.js
+++ 07_delete.js
@@ -1,33 +1,33 @@
-const express=require("express")
-const joi=require("@hapi/joi")
-const app=express()
-let movies=[
-    {id:1,name:"kgf",genre:"action"},
-    {id:2,name:"thiru",genre:"emotional"},
-    {id:3,name:"don",genre:"comedy"}
-]
-app.use(express.json())
-app.get("/api/movies",(req,res)=>{
-    res.send(movies)
-})
-app.delete("/api/movies/:id",(req,res)=>{
-    console.log("delete method")
-    const idvalidate=joi.object({id:joi.number().integer()})
-    const result=idvalidate.validate(req.params)
-    if(result.error){
-        console.log("error 1")
-        res.status(400).send(result.error.details[0].message)
-        return ;
-    }
-    const movie=movies.find(m=>m.id===parseInt(req.params.id))
-    if(!movie){
-        console.log("error 2")
+const express = require("express");
+const joi = require("@hapi/joi");
+const app = express();
+let movies = [
+  { id: 1, name: "kgf", genre: "action" },
+  { id: 2, name: "thiru", genre: "emotional" },
+  { id: 3, name: "don", genre: "comedy" },
+];
+app.use(express.json());
+app.get("/api/movies", (req, res) => {
+  res.send(movies);
+});
+app.delete("/api/movies/:id", (req, res) => {
+  console.log("delete method");
+  const idvalidate = joi.object({ id: joi.number().integer() });
+  const result = idvalidate.validate(req.params);
+  if (result.error) {
+    console.log("error 1");
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const movie = movies.find((m) => m.id === parseInt(req.params.id));
+  if (!movie) {
+    console.log("error 2");
 
-        res.send("No movie found")
-        return
-    }
-    let index=movies.indexOf(movie)
-    movies.splice(index,1)
-    res.send(movie)
-})
-app.listen(process.env.port||"5000")
+    res.send("No movie found");
+    return;
+  }
+  let index = movies.indexOf(movie);
+  movies.splice(index, 1);
+  res.send(movie);
+});
+app.listen(process.env.port || "5000");
